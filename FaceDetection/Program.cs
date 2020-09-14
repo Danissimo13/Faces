7d69cb8e822606386e92ba5870f9e7c252898e81
Danissimo13 <danissimokuvshin13@gmail.com>
2020-09-14 13:42:03 +0300

    Add example for change faces

--- FaceDetection/Program.cs
+++ FaceDetection/Program.cs
@@ -1,34 +1,23 @@
 ﻿using System;
+using System.Drawing.Text;
 using System.IO;
 using DlibDotNet;
-using FaceDetection.Services;
+using FaceDetection.Core;
 
 namespace FaceDetection
 {
     class Program
     {
+        private static ModelPathSystem pathSystem;
+
         public static void Main(string[] args) // Example use model
         {
             try
             {
-                ModelPathSystem pathSystem = new ModelPathSystem("../../../Assets/Input", "../../../Assets/Output"); // create path system with input and output pathes
-                string inputFileName = "1.jpg";
+                pathSystem = new ModelPathSystem("../../../Assets/Input", "../../../Assets/Output");
 
-                byte[] img;
-                using (FileStream reader = new FileStream(Path.Combine(pathSystem.InputsPath, inputFileName), FileMode.Open))
-                {
-                    img = new byte[reader.Length];
-                    reader.Read(img, 0, img.Length); // read image to bytes array
-                }
-
-                FaceRecognizer faceRecognizer = new FaceRecognizer(img, new RgbPixel(0, 255, 255), pathSystem); // create recognition model
-
-                var imageWithAllFaces = faceRecognizer.SaveOutlinedFacesImage("FirstTest", "main.jpg"); // save image with faces to Output/FirstTest/main.jpg
-                var imagePathes = faceRecognizer.SaveAllFacesImages("FirstTest"); // save all faces images to Output/FirstTest/ folder
-                foreach (string imagePath in imagePathes)
-                    Console.WriteLine($"===Face saved in {imagePath}===");
-                Console.WriteLine($"===Main image saved in {imageWithAllFaces}===");
-
+                TestDetect();
+                TestChange();
             }
             catch(Exception ex)
             {
@@ -37,6 +26,59 @@ namespace FaceDetection
 
             Console.WriteLine("===Press any key===");
             Console.ReadKey();
+        }
+
+        private static void TestDetect()
+        {
+             // create path system with input and output pathes
+            string inputFileName = "1.jpg";
+
+            byte[] img;
+            using (FileStream reader = new FileStream(Path.Combine(pathSystem.InputsPath, inputFileName), FileMode.Open))
+            {
+                img = new byte[reader.Length];
+                reader.Read(img, 0, img.Length); // read image to bytes array
+            }
+
+            FaceRecognizer faceRecognizer = new FaceRecognizer(img, new RgbPixel(0, 255, 255), pathSystem); // create recognition model
+
+            var imageWithAllFaces = faceRecognizer.SaveOutlinedFacesImage("FirstTest", "main.jpg"); // save image with faces to Output/FirstTest/main.jpg
+            var imagePathes = faceRecognizer.SaveAllFacesImages("FirstTest"); // save all faces images to Output/FirstTest/ folder
+            foreach (string imagePath in imagePathes)
+                Console.WriteLine($"===Face saved in {imagePath}===");
+            Console.WriteLine($"===Main image saved in {imageWithAllFaces}===");
+        }
+
+        private static void TestChange()
+        {
+            string fromImageName = "1.jpg";
+            string toImageName = "2.jpg";
+            string outputImageName = "deepFake.jpg";
+
+            Console.WriteLine("===Reading from image file===");
+            byte[] fromImage;
+            using(FileStream reader = new FileStream(Path.Combine(pathSystem.InputsPath, fromImageName), FileMode.Open))
+            {
+                fromImage = new byte[reader.Length];
+                reader.Read(fromImage, 0, fromImage.Length);
+            }
+
+            Console.WriteLine("===Reading to image file===");
+            byte[] toImage;
+            using (FileStream reader = new FileStream(Path.Combine(pathSystem.InputsPath, toImageName), FileMode.Open))
+            {
+                toImage = new byte[reader.Length];
+                reader.Read(toImage, 0, toImage.Length);
+            }
+
+            Console.WriteLine("===Replacing faces===");
+            byte[] changedImage = FaceReplacer.ReplaceFaces(fromImage, toImage);
+
+            Console.WriteLine("===Saving replaced faces image===");
+            using (FileStream reader = new FileStream(Path.Combine(pathSystem.OutputsPath, outputImageName), FileMode.OpenOrCreate))
+            {
+                reader.Write(changedImage);
+            }
         }
     }
 }
