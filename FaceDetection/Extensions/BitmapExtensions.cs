adef549953955205eea48a67362d14cbb42f0efb
Danissimo13 <danissimokuvshin13@gmail.com>
2020-09-14 13:40:56 +0300

    Add bitmap extensions

---
+++ FaceDetection/Extensions/BitmapExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace FaceDetection.Extensions
+{
+    internal static class BitmapExtensions
+    {
+        public static Bitmap ToBitmap(this byte[] image)
+        {
+            Bitmap bitmap;
+            using (MemoryStream reader = new MemoryStream(image))
+            {
+                Image img = Image.FromStream(reader);
+                bitmap = new Bitmap(img);
+            }
+
+            return bitmap;
+        }
+
+        public static byte[] ToByteArray(this Bitmap bitmap)
+        {
+            using (MemoryStream reader = new MemoryStream())
+            {
+                Image img = Image.FromHbitmap(bitmap.GetHbitmap());
+                img.Save(reader, ImageFormat.Jpeg);
+
+                return reader.ToArray();
+            }
+        }
+    }
+}
