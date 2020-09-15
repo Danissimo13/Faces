313d6150c31507ab4adec0cbb1d633d86c8d8cbb
Danissimo13 <danissimokuvshin13@gmail.com>
2020-09-15 14:56:44 +0300

    Some changes in program cs file

--- FaceDetection/Program.cs
+++ FaceDetection/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Drawing.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using DlibDotNet;
 using FaceDetection.Core;
@@ -31,7 +32,7 @@ namespace FaceDetection
         private static void TestDetect()
         {
              // create path system with input and output pathes
-            string inputFileName = "1.jpg";
+            string inputFileName = "from.jpg";
 
             byte[] img;
             using (FileStream reader = new FileStream(Path.Combine(pathSystem.InputsPath, inputFileName), FileMode.Open))
@@ -51,33 +52,17 @@ namespace FaceDetection
 
         private static void TestChange()
         {
-            string fromImageName = "1.jpg";
-            string toImageName = "2.jpg";
+            string fromImageName = "from.jpg";
+            string toImageName = "to.jpg";
             string outputImageName = "deepFake.jpg";
 
-            Console.WriteLine("===Reading from image file===");
-            byte[] fromImage;
-            using(FileStream reader = new FileStream(Path.Combine(pathSystem.InputsPath, fromImageName), FileMode.Open))
-            {
-                fromImage = new byte[reader.Length];
-                reader.Read(fromImage, 0, fromImage.Length);
-            }
-
-            Console.WriteLine("===Reading to image file===");
-            byte[] toImage;
-            using (FileStream reader = new FileStream(Path.Combine(pathSystem.InputsPath, toImageName), FileMode.Open))
-            {
-                toImage = new byte[reader.Length];
-                reader.Read(toImage, 0, toImage.Length);
-            }
 
             Console.WriteLine("===Replacing faces===");
-            byte[] changedImage = FaceReplacer.ReplaceFaces(fromImage, toImage);
-
+            Bitmap changedImage = FaceReplacer.ReplaceFaces(Path.Combine(pathSystem.InputsPath, fromImageName), Path.Combine(pathSystem.InputsPath, toImageName));
             Console.WriteLine("===Saving replaced faces image===");
             using (FileStream reader = new FileStream(Path.Combine(pathSystem.OutputsPath, outputImageName), FileMode.OpenOrCreate))
             {
-                reader.Write(changedImage);
+                changedImage.Save(reader, ImageFormat.Jpeg);
             }
         }
     }
