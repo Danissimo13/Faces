0d70c14b7fce946ce645d8fd81981bc52361d842
Danissimo13 <danissimokuvshin13@gmail.com>
2020-09-15 15:24:49 +0300

    Add web root

--- FacesWebApi/Program.cs
+++ FacesWebApi/Program.cs
@@ -21,6 +21,7 @@ namespace FacesWebApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseWebRoot("wwwroot");
                 });
     }
 }
