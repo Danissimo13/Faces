f99a1e884dcb908ee8f71dd06f93c7ac503905f3
Danissimo13 <danissimokuvshin13@gmail.com>
2020-09-18 17:57:02 +0300

    Add notify event about change url for spa navigate system

---
+++ FacesWebApi/wwwroot/js/navigate.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+(function () {
+    function onroutechange(oldRoute, newRoute) {
+        var oldSelector = '#navbar nav a[href="' + oldRoute + '"]';
+        var newSelector = '#navbar nav a[href="' + newRoute + '"]';
+
+        $(oldSelector).removeClass('active');
+        $(newSelector).addClass('active');
+    }
+
+    function init() {
+        var router = new Router([
+            new Route('home', 'home.html', true),
+            new Route('about', 'about.html')
+        ]);
+        router.onroutechange = onroutechange;
+
+        router.init();
+    }
+
+    init();
+}());
