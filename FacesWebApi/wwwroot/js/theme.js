80c5df49b898507cc1e8f6b51bfade6a548ff215
Danissimo13 <danissimokuvshin13@gmail.com>
2020-09-20 23:10:00 +0300

    Add theme js file for switch theme

---
+++ FacesWebApi/wwwroot/js/theme.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+	const switcher = $('#theme-switcher');
+
+	if ($.cookie('theme')) {
+		document.documentElement.setAttribute("theme", "dark");
+		switcher.addClass('active');
+    }
+
+	switcher.on('click', () => {
+
+		if (document.documentElement.hasAttribute('theme')) {
+			document.documentElement.removeAttribute("theme");
+			$.removeCookie('theme');
+			switcher.removeClass('active');
+		}
+		else {
+			document.documentElement.setAttribute("theme", "dark");
+			$.cookie('theme', 'dark');
+			switcher.addClass('active');
+        }
+	});
+});
