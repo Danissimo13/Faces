e2a2cbbb2a29a2cf2f8acd6b774c9f542c051dfa
Danissimo13 <danissimokuvshin13@gmail.com>
2020-09-22 21:30:47 +0300

    Add implementation Repository pattern with MSSql database

---
+++ FacesStorage.Data.MSSql/StorageContext.cs
@@ -0,0 +1,39 @@
+﻿using FacesStorage.Data.Abstractions;
+using FacesStorage.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FacesStorage.Data.MSSql
+{
+    public class StorageContext : DbContext, IStorageContext
+    {
+        private readonly string connectionString;
+
+        public StorageContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>(etb =>
+            {
+                etb.HasKey(e => e.UserId);
+                etb.Property(e => e.UserId);
+                etb.Property(e => e.Email);
+                etb.Property(e => e.PasswordId);
+                etb.Property(e => e.RoleId);
+                etb.ToTable("Users");
+            });
+            modelBuilder.Entity<User>().HasOne(e => e.Password).WithOne().HasForeignKey<User>(e => e.PasswordId);
+            modelBuilder.Entity<User>().HasOne(e => e.Role).WithMany().HasForeignKey(e => e.RoleId);
+        }
+    }
+}
---
+++ FacesStorage.Data.MSSql/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using FacesStorage.Data.Abstractions;
+using FacesStorage.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FacesStorage.Data.MSSql
+{
+    class UserRepository : IUserRepository
+    {
+        private StorageContext storageContext;
+        private DbSet<User> dbSet;
+
+        public IQueryable<User> All()
+        {
+            return dbSet.AsQueryable<User>();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            User user = await dbSet.FirstOrDefaultAsync(u => u.UserId == id);
+            if (user == null) throw new KeyNotFoundException($"Not found user with id equal {id}");
+
+            return user;
+        }
+
+        public async Task<User> CreateAsync(User user)
+        {
+            var entityEntry = await dbSet.AddAsync(user);
+            return entityEntry.Entity;
+        }
+
+        public User Edit(User user)
+        {
+            var entityEntry = dbSet.Update(user);
+            return entityEntry.Entity;
+        }
+
+        public void Delete(User user)
+        {
+            dbSet.Remove(user);
+        }
+
+        public void SetStorageContext(IStorageContext storageContext)
+        {
+            this.storageContext = storageContext as StorageContext;
+            this.dbSet = this.storageContext.Set<User>();
+        }
+    }
+}
