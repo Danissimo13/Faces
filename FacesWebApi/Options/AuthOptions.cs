25dc6607447251fbc217ff203b59178c13bca881
Danissimo13 <danissimokuvshin13@gmail.com>
2020-09-23 23:31:44 +0300

    Add authentication, storage context and hash service to service collection

---
+++ FacesWebApi/Options/AuthOptions.cs
@@ -0,0 +1,24 @@
+﻿using FacesWebApi.Services.Implemetations;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FacesWebApi.Options
+{
+    public static class AuthOptions
+    {
+        public const string ISSUER = "FacesCorporation";
+        public const string AUDIENCE = "FacesApp";
+        public const int LIFETIME = 60;
+
+        private static readonly byte[] KEY;
+
+        static AuthOptions()
+        {
+            KEY = new HashService().Key;
+        }
+
+        public static SymmetricSecurityKey GetSymmetricSecurity()
+        {
+            return new SymmetricSecurityKey(KEY);
+        }
+    }
+}
