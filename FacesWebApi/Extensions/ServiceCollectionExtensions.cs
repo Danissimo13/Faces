2b0a363fa765dd28507b35ecdd303bcbf6ea240b
Danissimo13 <danissimokuvshin13@gmail.com>
2020-09-23 11:18:33 +0300

    Add new service extensions
    Add storage context extension
    Add hash service extension

--- FacesWebApi/Extensions/ServiceCollectionExtensions.cs
+++ FacesWebApi/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,10 @@
 ﻿using FaceDetection.Core;
+using FacesStorage.Data.Abstractions;
+using FacesStorage.Data.MSSql;
+using FacesWebApi.Services.Abstractions;
+using FacesWebApi.Services.Implemetations;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.IO;
 using System.Linq;
@@ -18,6 +23,24 @@ namespace FacesWebApi.Extensions
             string outputFacesPath = Path.Combine(environment.WebRootPath, "faces", "responses");
             ModelPathSystem pathSystem = new ModelPathSystem(inputFacesPath, outputFacesPath);
             services.AddSingleton<ModelPathSystem>(pathSystem);
+
+            return services;
+        }
+
+        public static IServiceCollection AddStorageContext(this IServiceCollection services)
+        {
+            services.AddScoped<IStorage, Storage>(provider =>
+            {
+                IConfiguration config = provider.GetRequiredService<IConfiguration>();
+                return new Storage(config.GetConnectionString("DbConnection"));
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddHashService(this IServiceCollection services)
+        {
+            services.AddSingleton<IHashService, HashService>();
 
             return services;
         }
