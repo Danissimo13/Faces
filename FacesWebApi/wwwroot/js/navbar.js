af5f43ae7f65a2440c8c4dab4dae23e89afbcc6e
Danissimo13 <danissimokuvshin13@gmail.com>
2020-09-26 15:23:14 +0300

    Add switch nav bar for phones

---
+++ FacesWebApi/wwwroot/js/navbar.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    var isHide = true;
+
+    $('#nav-switcher').on('click', async function () {
+        if (isHide) {
+            $('#references').removeClass('nav-hidden');
+            isHide = false;
+        }
+        else {
+            $('#references').addClass('nav-hidden');
+            isHide = true;
+        }
+    });
+});
