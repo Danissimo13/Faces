b6b69d124fe87a3a8588fb7448e23cbb39a5ac70
Danissimo13 <danissimokuvshin13@gmail.com>
2020-09-30 20:51:30 +0300

    Some changes in login page styles and script

--- FacesWebApi/wwwroot/js/login.js
+++ FacesWebApi/wwwroot/js/login.js
@@ -1,6 +1,9 @@
 ﻿$(function () {
     const login_inp = $('#login-form #email');
     const password_inp = $('#login-form #password');
+
+    const login_message = $('#login-form #login-message');
+
     $('#login-btn').on('click', async function (e) {
         e.preventDefault();
 
@@ -17,30 +20,30 @@
 
     function succeed_req(data) {
         localStorage.setItem('token', data.access_token);
-        window.location.hash = '#acc';
+
+        login_message.text(data.message);
+        login_message.removeClass('error');
+        login_message.addClass('succeed');
+
+        setTimeout(() => window.location.hash = 'acc', 2000);
     }
 
     function error_req(data) {
         console.log("Error: ", data.errorText);
-        $('#login-form #login-validate').text(data.errorText);
+
+        login_message.text(data.errorText);
+        login_message.removeClass('succeed');
+        login_message.addClass('error');
     }
 
     function validate_login_form() {
-        if (!login_inp.val()) {
-            login_inp.css('border-color', 'var(--danger-color)');
-            return false;
-        }
-        else {
-            login_inp.css('border-color', 'var(--violet-color)');
-        }
+        if (!login_inp.val()) login_inp.addClass('non-valid');
+        else login_inp.removeClass('non-valid');
 
-        if (!password_inp.val()) {
-            password_inp.css('border-color', 'var(--danger-color)');
-            return false;
-        }
-        else {
-            password_inp.css('border-color', 'var(--violet-color)');
-        }
+        if (!password_inp.val()) password_inp.addClass('non-valid');
+        else password_inp.removeClass('non-valid');
+
+        if (login_inp.hasClass('non-valid') || password_inp.hasClass('non-valid')) return false;
 
         return true;
     }
