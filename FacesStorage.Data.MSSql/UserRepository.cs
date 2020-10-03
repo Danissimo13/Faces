1e34572f30ed602e86183d5a4303eeeb25b332c9
Danissimo13 <danissimokuvshin13@gmail.com>
2020-10-04 01:31:19 +0300

    Change user search system in user repository

--- FacesStorage.Data.MSSql/UserRepository.cs
+++ FacesStorage.Data.MSSql/UserRepository.cs
@@ -1,6 +1,8 @@
 ﻿using FacesStorage.Data.Abstractions;
+using FacesStorage.Data.Abstractions.SearchOptions;
 using FacesStorage.Data.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -13,30 +15,116 @@ namespace FacesStorage.Data.MSSql
         private StorageContext storageContext;
         private DbSet<User> userDbSet;
 
-        public IQueryable<User> All()
+        public IList<User> All(Action<UsersSearchOptions> searchOptionsBuilder)
         {
-            return userDbSet.AsQueryable<User>();
+            UsersSearchOptions searchOptions = new UsersSearchOptions();
+            searchOptionsBuilder(searchOptions);
+
+            IQueryable<User> users = userDbSet.AsQueryable<User>();
+
+
+            if (searchOptions.From.HasValue)
+                users = users.Skip(searchOptions.From.Value);
+            if (searchOptions.Count.HasValue)
+                users = users.Take(searchOptions.Count.Value);
+
+            if (searchOptions.WithRole)
+                users = users.Include(u => u.Role);
+            if (searchOptions.WithRequests)
+                users = users.Include(u => u.Requests);
+            if (searchOptions.WithRequestImages)
+                users = users.Include(u => u.Requests).ThenInclude(r => r.Images);
+            if (searchOptions.WithRequestResponses)
+                users = users.Include(u => u.Requests).ThenInclude(r => r.Response);
+            if (searchOptions.WithResponseImages)
+                users = users.Include(u => u.Requests).ThenInclude(r => r.Response).ThenInclude(r => r.Images);
+
+            users = users.Select(u => new User()
+            {
+                UserId = u.UserId,
+                Email = u.Email,
+                Nickname = u.Nickname,
+                Role = u.Role,
+                Requests = u.Requests,
+                Password = searchOptions.WithPassword ? u.Password : null
+            });
+
+            switch (searchOptions.SearchType)
+            {
+                case UsersSearchTypes.ByEmail:
+                    users = users.Where(u => u.Email.Contains(searchOptions.Email));
+                    break;
+                case UsersSearchTypes.ByNickname:
+                    users = users.Where(u => u.Nickname.Contains(searchOptions.Nickname));
+                    break;
+                case UsersSearchTypes.WithoutProperty:
+                    break;
+            }
+            
+            var usersList = users.AsEnumerable().Select(u =>
+            {
+                if(searchOptions.FromRequest.HasValue)
+                    u.Requests = u.Requests.Skip(searchOptions.FromRequest.Value).ToList();
+                if (searchOptions.RequestsCount.HasValue)
+                    u.Requests = u.Requests.Take(searchOptions.RequestsCount.Value).ToList();
+                return u;
+            }).ToList();
+
+            return usersList;
         }
 
-        public async Task<User> GetByIdAsync(int id)
+        public async Task<User> GetAsync(Action<UserSearchOptions> searchOptionsBuilder)
         {
-            User user = await userDbSet
-                .Include(u => u.Role)
-                .Include(u => u.Requests)
-                .FirstOrDefaultAsync(u => u.UserId == id);
-            if (user == null) throw new KeyNotFoundException($"Not found user with id equal {id}.");
+            UserSearchOptions searchOptions = new UserSearchOptions();
+            searchOptionsBuilder(searchOptions);
 
-            return user;
-        }
+            IQueryable<User> users = userDbSet.AsQueryable<User>();
 
-        public async Task<User> GetByEmailAsync(string email)
-        {
+            if (searchOptions.WithRole) 
+                users = users.Include(u => u.Role);
+            if (searchOptions.WithRequests) 
+                users = users.Include(u => u.Requests);
+            if (searchOptions.WithRequestImages) 
+                users = users.Include(u => u.Requests).ThenInclude(r => r.Images);
+            if (searchOptions.WithRequestResponses) 
+                users = users.Include(u => u.Requests).ThenInclude(r => r.Response);
+            if (searchOptions.WithResponseImages) 
+                users = users.Include(u => u.Requests).ThenInclude(r => r.Response).ThenInclude(r => r.Images);
 
-            User user = await userDbSet
-                .Include(u => u.Role)
-                .Include(u => u.Requests)
-                .FirstOrDefaultAsync(u => u.Email == email);
-            if (user == null) throw new KeyNotFoundException($"Not found user with email equal {email}.");
+            users = users.Select(u => new User()
+            {
+                UserId = u.UserId,
+                Email = u.Email,
+                Nickname = u.Nickname,
+                Role = u.Role,
+                Requests = u.Requests,
+                Password = searchOptions.WithPassword ? u.Password : null
+            });
+
+            User user = null;
+            switch (searchOptions.SearchType)
+            {
+                case UserSearchTypes.ById:
+                    user = await users.FirstOrDefaultAsync(u => u.UserId == searchOptions.UserId);
+                    if (user == null) throw new KeyNotFoundException($"Not found user with id equal {searchOptions.UserId}.");
+                    break;
+                case UserSearchTypes.ByEmail:
+                    user = await users.FirstOrDefaultAsync(u => u.Email == searchOptions.Email);
+                    if (user == null) throw new KeyNotFoundException($"Not found user with email equal {searchOptions.Email}.");
+                    break;
+                case UserSearchTypes.ByNickname:
+                    user = await users.FirstOrDefaultAsync(u => u.Nickname == searchOptions.Nickname);
+                    if (user == null) throw new KeyNotFoundException($"Not found user with nickname equal {searchOptions.Nickname}.");
+                    break;
+                case UserSearchTypes.WithoutProperty:
+                    break;
+            }
+
+            if(searchOptions.FromRequest.HasValue)
+                user.Requests = user.Requests.Skip(searchOptions.FromRequest.Value).ToList();
+            if (searchOptions.RequestsCount.HasValue)
+                user.Requests = user.Requests.Take(searchOptions.RequestsCount.Value).ToList();
+
             return user;
         }
 
