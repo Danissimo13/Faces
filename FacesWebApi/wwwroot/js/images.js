a63784a18ac3744e411b35e3dc7e5c72c5ece4e0
Danissimo13 <danissimokuvshin13@gmail.com>
2020-10-04 01:35:26 +0300

    Add images.js for work with images

---
+++ FacesWebApi/wwwroot/js/images.js
@@ -0,0 +1,14 @@
+﻿function scope(e) {
+    var scopedImgHtml = '<div id="scoped-image"></div>';
+    var blockImgHtml = '<div id="scoped-image-block"><img class="inverted" src="' + $(e).attr('src') + '" /></div>';
+    var scopedImg = $(scopedImgHtml);
+    var blockImg = $(blockImgHtml);
+
+    scopedImg.append(blockImg);
+    $('body').append(scopedImg);
+
+    scopedImg.on('click', function (e) {
+        if (e.target.id == scopedImg.attr('id'))
+            $(scopedImg).remove();
+    });
+};
