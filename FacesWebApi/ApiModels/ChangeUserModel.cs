34bff85b239ee3fb0c5489c9b49d4f9397d2d5f8
Danissimo13 <danissimokuvshin13@gmail.com>
2020-10-04 01:35:01 +0300

    Add validations for api requests models

---
+++ FacesWebApi/ApiModels/ChangeUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FacesWebApi.ApiModels
+{
+    public class ChangeUserModel
+    {
+        [Required(ErrorMessage = "You didn't enter your nickname.")]
+        [MinLength(5, ErrorMessage = "Minimal lenght for nickname is 5.")]
+        public string Nickname { get; set; }
+
+        [Required(ErrorMessage = "You didn't enter your email address.")]
+        [EmailAddress(ErrorMessage = "Email address had a non-valid format.")]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
