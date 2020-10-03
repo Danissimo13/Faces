7e0338cec6038fe897658cbdc95bdad74883f33a
Danissimo13 <danissimokuvshin13@gmail.com>
2020-10-04 01:35:45 +0300

    Add validation.js for server side validations

---
+++ FacesWebApi/wwwroot/js/validation.js
@@ -0,0 +1,15 @@
+﻿function displayModelErrors(modelState, infoBox) {
+    var errors = '';
+    for (var key in modelState.errors) {
+        for (var error of modelState.errors[key]) {
+            console.error('Error: ' + error);
+            errors += error + '</br>';
+        }
+    }
+
+    if (infoBox && errors) {
+        infoBox.removeClass('succeed');
+        infoBox.addClass('error');
+        infoBox.html(errors);
+    }
+}
