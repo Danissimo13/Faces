c95eb5368ecfeeae78d65e3c50957839b3bfc0cb
Danissimo13 <danissimokuvshin13@gmail.com>
2020-10-04 23:50:10 +0300

    Add api model SearchUserModel.cs

---
+++ FacesWebApi/ApiModels/SearchUserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FacesWebApi.ApiModels
+{
+    public class SearchUserModel
+    {
+        public bool WithRole { get; set; }
+
+        public bool WithRequests { get; set; }
+
+        public int? FromRequest { get; set; }
+        public int? RequestsCount { get; set; }
+
+        public bool WithRequestImages { get; set; }
+
+        public bool WithRequestResponses { get; set; }
+
+        public bool WithResponseImages { get; set; }
+    }
+}
