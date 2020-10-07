9648e090645b72d5f7aca27358eb5b18a0cfc230
Danissimo13 <danissimokuvshin13@gmail.com>
2020-10-07 16:45:55 +0300

    Add news block to home page

---
+++ FacesWebApi/wwwroot/js/home.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    api_get('news', null, null, succeed_news_request, error_news_request)
+
+    function succeed_news_request(data) {
+        var newsHref = 'news?id=' + data.id;
+        $('#last-news').attr('href', newsHref);
+        $('#last-news-topic').text(data.topic);
+        $('#news-publish-date').text(data.publishDate);
+        $('#last-news img').attr('src', data.imageSrc);
+        $('#last-news').css('display', 'flex');
+    }
+
+    function error_news_request(data) {
+        displayModelErrors(data);
+        $('#last-news').remove();
+    }
+});
