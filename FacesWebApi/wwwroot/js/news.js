d6b5a976ba441b0645bd04efeb88831c284a8bdf
Danissimo13 <danissimokuvshin13@gmail.com>
2020-10-07 17:03:14 +0300

    Add news page with js handler

---
+++ FacesWebApi/wwwroot/js/news.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    const news_topic = $('#news-topic');
+    const news_image = $('#news-img');
+    const news_body = $('#news-body');
+    const news_publish_date = $('#news-publish-date');
+
+    const url = new window.URL(location.href);
+    const id = url.searchParams.get('id');
+    api_get('news', id, [], succeed_req, error_req);
+
+    function succeed_req(data) {
+        news_topic.text(data.topic);
+        news_image.attr('src', data.imageSrc);
+        news_body.text(data.body);
+        news_publish_date.text(data.publishDate);
+    }
+
+    function error_req(data) {
+        displayModelErrors(data);
+        notFound();
+    }
+});
