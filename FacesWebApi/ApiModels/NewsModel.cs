7e6d99082feb54f701bfe8a268cda8f15eae061e
Danissimo13 <danissimokuvshin13@gmail.com>
2020-10-07 16:47:15 +0300

    Add api news model for creating news

---
+++ FacesWebApi/ApiModels/NewsModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace FacesWebApi.ApiModels
+{
+    public class NewsModel
+    {
+        [Required(ErrorMessage = "You didn't enter a title.")]
+        public string Topic { get; set; }
+
+        [Required(ErrorMessage = "You didn't enter a body.")]
+        public string Body { get; set; }
+
+        [Required(ErrorMessage = "You didn't choose a image.")]
+        public IFormFile Image { get; set; }
+    }
+}
