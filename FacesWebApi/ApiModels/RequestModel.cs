6ccbdd6e882fea6e9f470ef05bee966ad3a613af
Danissimo13 <danissimokuvshin13@gmail.com>
2020-10-10 23:10:39 +0300

    Add faces request api model

---
+++ FacesWebApi/ApiModels/RequestModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace FacesWebApi.ApiModels
+{
+    public class RequestModel
+    {
+        [Required(ErrorMessage = "You didnt enter a request type.")]
+        public string RequestType { get; set; }
+
+        [Required(ErrorMessage = "You didnt load an image.")]
+        public IFormFile FromImage { get; set; }
+
+        public IFormFile ToImage { get; set; }
+    }
+}
