05ace7fefec490cf4c673f946458c9535af5d357
Danissimo13 <danissimokuvshin13@gmail.com>
2020-10-17 12:05:37 +0300

    Change return type from TRequest and TResponse to Request and Response

--- FacesStorage.Data.Abstractions/IRequestRepository.cs
+++ FacesStorage.Data.Abstractions/IRequestRepository.cs
@@ -7,7 +7,7 @@ namespace FacesStorage.Data.Abstractions
     public interface IRequestRepository : IRepository
     {
         public IQueryable<TRequest> All<TRequest>() where TRequest : Request;
-        public Task<TRequest> GetByIdAsync<TRequest>(int id) where TRequest : Request;
+        public Task<Request> GetByIdAsync(int id);
         public Task<Request> CreateAsync(Request request);
         public Request Edit(Request request);
         public void Delete(Request request);
