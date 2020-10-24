9b23a48067cfe9460842055cf5485b8c2a542769
Ковалеров Ярослав <danissimokuvshin13@gmail.com>
2020-10-25 01:43:39 +0300

    Change get method last news in home.js

--- FacesWebApi/wwwroot/js/home.js
+++ FacesWebApi/wwwroot/js/home.js
@@ -1,13 +1,22 @@
 ﻿$(function () {
-    api_get('news', null, null, succeed_news_request, error_news_request)
+    var searchOptions = [
+        { name: "from", value: 0 },
+        { name: "count", value: 1 }
+    ]
+    api_get('news', null, searchOptions, succeed_news_request, error_news_request);
 
     function succeed_news_request(data) {
-        var newsHref = 'news?id=' + data.id;
-        $('#last-news').attr('href', newsHref);
-        $('#last-news-topic').text(data.topic);
-        $('#news-publish-date').text(data.publishDate);
-        $('#last-news img').attr('src', data.imageSrc);
-        $('#last-news').css('display', 'flex');
+        if (data && data[0]) {
+            var newsHref = 'news?id=' + data[0].id;
+            $('#last-news').attr('href', newsHref);
+            $('#last-news-topic').text(data[0].topic);
+            $('#news-publish-date').text(data[0].publishDate);
+            $('#last-news img').attr('src', data[0].imageSrc);
+            $('#last-news').css('display', 'flex');
+        }
+        else {
+            $('#last-news').remove();
+        }
     }
 
     function error_news_request(data) {
