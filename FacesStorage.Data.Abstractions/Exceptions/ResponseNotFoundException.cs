406e8fc02fa2fe76122f0b98dd80b66428512b20
Ковалеров Ярослав <danissimokuvshin13@gmail.com>
2020-10-25 01:37:34 +0300

    Add exceptions models to abstractions project

---
+++ FacesStorage.Data.Abstractions/Exceptions/ResponseNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FacesStorage.Data.Abstractions.Exceptions
+{
+    public class ResponseNotFoundException : Exception
+    {
+        public ResponseNotFoundException() { }
+        public ResponseNotFoundException(string message) : base(message) { }
+    }
+}
